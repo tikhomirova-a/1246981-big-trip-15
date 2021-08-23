1b9d39a1395cbecb8e64d3f58c7493dd22a9a8cc
tikhomirova-a <tikhomirova.aa95@gmail.com>
2021-08-23 22:11:38 +0300

    добавляет правки после ревью

--- src/utils/event.js
+++ src/utils/event.js
@@ -6,23 +6,10 @@ dayjs.extend(duration);
 export const getDuration = (start, end) => {
   const dateDifference = dayjs(end).diff(dayjs(start));
   const durationTime = dayjs.duration(dateDifference);
-  let durationDays;
-  let durationHours;
-  let durationMinutes;
-  if (durationTime.minutes() === 0) {
-    durationMinutes = '';
-  } else {
-    durationMinutes = durationTime.minutes() < 10 ? `0${durationTime.minutes()}M ` : `${durationTime.minutes()}M `;
-  }
-  if (durationTime.days() === 0) {
-    durationDays = '';
-  } else {
-    durationDays = durationTime.days() < 10 ? `0${durationTime.days()}D ` : `${durationTime.days()}D `;
-  }
-  if (durationTime.hours() === 0) {
-    durationHours = durationDays === '' ? '' : 'OOH ';
-  } else {
-    durationHours = durationTime.hours() < 10 ? `0${durationTime.hours()}H ` : `${durationTime.hours()}H `;
-  }
-  return `${durationDays + durationHours + durationMinutes}`;
+
+  const durationMinutes = durationTime.minutes() === 0 ? '' : durationTime.minutes().toString().padStart(2, '0').padEnd(4, 'M ');
+  const durationDays = durationTime.days() === 0 ? '' : durationTime.days().toString().padStart(2, '0').padEnd(4, 'D ');
+  const durationHours = durationTime.hours() === 0 && durationTime.days() === 0 ? '' : durationTime.hours().toString().padStart(2, '0').padEnd(4, 'H ');
+
+  return `${durationDays}${durationHours}${durationMinutes}`;
 };
